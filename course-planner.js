333efd5b3adcfbbb760c787fd9b80d4490efc7e4
alex-gru <grusch.alex@gmail.com>
2017-04-09 15:18:46 +0200

    implement nodejs backend in Typescript :) minor code refactoring (e.g. const/let vs var)

--- course-planner.js
+++ course-planner.js
@@ -1,52 +1,39 @@
-'use strict';
-
-const express = require('express');
-const mongoose = require("mongoose");
-const path = require('path');
-const pug = require('pug');
-
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var mongoose = require("mongoose");
+var path = require("path");
 // Constants
-const PORT = 8080;
-
+var port = 8080;
 // App
-const app = express();
-
+var app = express();
 // provide paths to static files
 app.use("/js", express.static(__dirname + '/public/js'));
 app.use("/views", express.static(__dirname + '/views'));
-
 // view engine setup
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
-
-app.get('/', function (req, res) {
-  Data.findOne(function (err, data) {
-    res.render('index', {title: "Course Planner", text: data.text});
-  })
-});
-
-var server = app.listen(PORT, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  log("Webapp listening at http://%s:%s", host, port)
-});
-
-
 var Data;
+app.get('/', function (req, res) {
+    Data.findOne(function (err, data) {
+        res.render('index', { title: "Course Planner", text: data.text });
+    });
+});
+var server = app.listen(port, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Webapp listening at http://%s:%s", host, port);
+});
 if (mongoose.connection.readyState === 0) {
-  /* 0 = disconnected, 1 = connected,  2 = connecting,  3 = disconnecting  */
-  mongoose.connect('mongodb://localhost:27017/db');
-  mongoose.connection.on('connected', function () {
-    log("Connection to MongoDB established.");
-    Data = mongoose.model('Data',
-        new mongoose.Schema({ text: String}),
-        'data');     // collection name
-  });
+    /* 0 = disconnected, 1 = connected,  2 = connecting,  3 = disconnecting  */
+    mongoose.connect('mongodb://localhost:27017/db');
+    mongoose.connection.on('connected', function () {
+        log("Connection to MongoDB established.");
+        Data = mongoose.model('Data', new mongoose.Schema({ text: String }), 'data'); // collection name
+    });
 }
-
-function log(object) {
-  var date = new Date();
-  var time = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
-  console.log(time + " - ", object);
+function log(message) {
+    var date = new Date();
+    var time = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
+    console.log(time + " - ", message);
 }
---
+++ course-planner.ts
@@ -0,0 +1,49 @@
+import * as express from "express";
+import * as mongoose from "mongoose";
+import * as path from "path";
+
+// Constants
+const port = 8080;
+
+// App
+const app = express();
+
+// provide paths to static files
+app.use("/js", express.static(__dirname + '/public/js'));
+app.use("/views", express.static(__dirname + '/views'));
+
+// view engine setup
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+let Data;
+app.get('/', (req, res) => {
+  Data.findOne((err, data) => {
+    res.render('index', {title: "Course Planner", text: data.text});
+  })
+});
+
+const server = app.listen(port, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log("Webapp listening at http://%s:%s", host, port)
+});
+
+
+if (mongoose.connection.readyState === 0) {
+  /* 0 = disconnected, 1 = connected,  2 = connecting,  3 = disconnecting  */
+  mongoose.connect('mongodb://localhost:27017/db');
+  mongoose.connection.on('connected', () => {
+    log("Connection to MongoDB established.");
+    Data = mongoose.model('Data',
+        new mongoose.Schema({text: String}),
+        'data');     // collection name
+  });
+}
+
+function log(message:any) {
+  const date = new Date();
+  const time = date.getDate() + "." + (date.getMonth() + 1) + "." + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes();
+  console.log(time + " - ",message);
+}
