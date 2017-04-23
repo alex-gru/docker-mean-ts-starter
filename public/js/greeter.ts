4b1684fcf253063756a202dec0d81ad15faca278
Alexander Gruschina <grusch.alex@gmail.com>
2017-04-23 16:47:14 +0200

    fix using let vs var

--- public/js/greeter.ts
+++ public/js/greeter.ts
@@ -1,7 +1,7 @@
-declare var angular: any;   // angular library included in pug
-declare var greeting: any;  // defined in pug (in fact a template variable fed in node backend)
+let angular: any;    // angular library included in pug
+let greeting: any;   // defined in pug (in fact a template variable fed in node backend)
 
-var app = angular.module("greetingApp", []);
+const app = angular.module("greetingApp", []);
 
 app.controller("greetingCtrl", ($scope) => {
     $scope.greeting = greeting;
